641f29a7972188f0a7e0bd7aa072633b9c920bcc
shallow90125 <shallow90125@gmail.com>
2023-06-22 15:51:17 +0900

    test

--- app/api/ababa/route.ts
+++ app/api/ababa/route.ts
@@ -3,7 +3,3 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   return NextResponse.json({ a: 123 });
 }
-
-export async function POST(request: Request) {
-  return new Response(null, { status: 200 });
-}
