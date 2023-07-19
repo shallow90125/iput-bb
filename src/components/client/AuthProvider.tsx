05781e196ed1f84a6182b53988ada66add558806
shallow90125 <shallow90125@gmail.com>
2023-07-20 02:24:08 +0900

    update AuthProvider

--- src/components/client/AuthProvider.tsx
+++ src/components/client/AuthProvider.tsx
@@ -1,23 +1,31 @@
 "use client";
 
-import { userAtom } from "@/utils/atom";
+import { initAtom, userAtom } from "@/utils/atom";
 import { initApp } from "@/utils/initApp";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { useSetRecoilState } from "recoil";
+import { useEffect, useTransition } from "react";
+import { useRecoilState, useSetRecoilState } from "recoil";
 
 export default function AuthProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  initApp();
-  const auth = getAuth();
-
+  const [isPending, startTransition] = useTransition();
   const setCurrentUser = useSetRecoilState(userAtom);
+  const [isInit, setIsInit] = useRecoilState(initAtom);
 
-  const unsubscribe = onAuthStateChanged(auth, (user) => setCurrentUser(user));
-
-  // unsubscribe();
+  useEffect(() => {
+    startTransition(() => {
+      initApp();
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        console.log(`AuthProvider: onAuthStateChanged: ${user?.email}`);
+        setCurrentUser(user);
+        if (!isInit) setIsInit(true);
+      });
+    });
+  }, []);
 
   return <>{children}</>;
 }
--- src/utils/atom.ts
+++ src/utils/atom.ts
@@ -6,3 +6,18 @@ export const userAtom = atom<User | null>({
   default: null,
   dangerouslyAllowMutability: true,
 });
+
+export const sensorsAtom = atom<Sensor[]>({
+  key: "sensors",
+  default: [],
+});
+
+export const authSpinnerAtom = atom<boolean>({
+  key: "authSpinner",
+  default: false,
+});
+
+export const initAtom = atom<boolean>({
+  key: "init",
+  default: false,
+});
