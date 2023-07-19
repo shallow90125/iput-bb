5d2a2dad4d5335f1a8073afbf9db51c6b27b2e98
shallow90125 <shallow90125@gmail.com>
2023-07-20 03:11:06 +0900

    update with AuthRedirector

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,15 +13,14 @@ import {
   Text,
   useToast,
 } from "@/components/chakra-ui/react";
-import { userAtom } from "@/utils/atom";
+import AuthRedirector from "@/components/client/AuthRedirector";
 import { initApp } from "@/utils/initApp";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { useRecoilValue } from "recoil";
 
 type Form = {
   email: string;
@@ -36,94 +35,91 @@ export default function Login() {
   } = useForm<Form>();
 
   const [isShow, setIsShow] = useState(false);
-  const user = useRecoilValue(userAtom);
 
   initApp();
   const auth = getAuth();
   const router = useRouter();
   const toast = useToast();
 
-  useEffect(() => {
-    if (user) router.replace("/");
-  });
-
   return (
-    <SimpleGrid
-      as="form"
-      placeContent="center"
-      placeItems="center"
-      gap={4}
-      p={4}
-      onSubmit={handleSubmit(async (data) => {
-        try {
-          await signInWithEmailAndPassword(auth, data.email, data.password);
-          toast({
-            title: "正常にログインされました",
-            status: "success",
-            duration: 5000,
-            isClosable: true,
-          });
-          router.push("/");
-        } catch (error) {
-          toast({
-            title: `${error}`,
-            status: "error",
-            duration: 5000,
-            isClosable: true,
-          });
-        }
-      })}
-    >
-      <FormControl isInvalid={!!errors.email}>
-        <FormLabel>メールアドレス</FormLabel>
-        <Input
-          type="email"
-          placeholder="user@example.com"
-          {...register("email", {
-            required: "必須項目",
-          })}
-        ></Input>
-        {errors.email && (
-          <FormErrorMessage>{errors.email.message}</FormErrorMessage>
-        )}
-      </FormControl>
-      <FormControl isInvalid={!!errors.password}>
-        <FormLabel>パスワード</FormLabel>
-        <InputGroup>
+    <AuthRedirector isExists={true}>
+      <SimpleGrid
+        as="form"
+        placeContent="center"
+        placeItems="center"
+        gap={4}
+        p={4}
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await signInWithEmailAndPassword(auth, data.email, data.password);
+            toast({
+              title: "正常にログインされました",
+              status: "success",
+              duration: 5000,
+              isClosable: true,
+            });
+            router.push("/");
+          } catch (error) {
+            toast({
+              title: `${error}`,
+              status: "error",
+              duration: 5000,
+              isClosable: true,
+            });
+          }
+        })}
+      >
+        <FormControl isInvalid={!!errors.email}>
+          <FormLabel>メールアドレス</FormLabel>
           <Input
-            maxLength={16}
-            type={isShow ? "text" : "password"}
-            {...register("password", {
+            type="email"
+            placeholder="user@example.com"
+            {...register("email", {
               required: "必須項目",
-              minLength: {
-                value: 8,
-                message: "8~16文字",
-              },
-              maxLength: {
-                value: 16,
-                message: "8~16文字",
-              },
             })}
           ></Input>
-          <InputRightElement placeContent="center" placeItems="center">
-            <IconButton
-              variant="unstyled"
-              aria-label="show"
-              placeContent="center"
-              placeItems="center"
-              icon={isShow ? <ViewIcon /> : <ViewOffIcon />}
-              onClick={() => setIsShow(!isShow)}
-            />
-          </InputRightElement>
-        </InputGroup>
-        {errors.password && (
-          <FormErrorMessage>{errors.password.message}</FormErrorMessage>
-        )}
-      </FormControl>
-      <Button type="submit">Login</Button>
-      <Link href="/register">
-        <Text>またはアカウントを登録する</Text>
-      </Link>
-    </SimpleGrid>
+          {errors.email && (
+            <FormErrorMessage>{errors.email.message}</FormErrorMessage>
+          )}
+        </FormControl>
+        <FormControl isInvalid={!!errors.password}>
+          <FormLabel>パスワード</FormLabel>
+          <InputGroup>
+            <Input
+              maxLength={16}
+              type={isShow ? "text" : "password"}
+              {...register("password", {
+                required: "必須項目",
+                minLength: {
+                  value: 8,
+                  message: "8~16文字",
+                },
+                maxLength: {
+                  value: 16,
+                  message: "8~16文字",
+                },
+              })}
+            ></Input>
+            <InputRightElement placeContent="center" placeItems="center">
+              <IconButton
+                variant="unstyled"
+                aria-label="show"
+                placeContent="center"
+                placeItems="center"
+                icon={isShow ? <ViewIcon /> : <ViewOffIcon />}
+                onClick={() => setIsShow(!isShow)}
+              />
+            </InputRightElement>
+          </InputGroup>
+          {errors.password && (
+            <FormErrorMessage>{errors.password.message}</FormErrorMessage>
+          )}
+        </FormControl>
+        <Button type="submit">Login</Button>
+        <Link href="/register">
+          <Text>またはアカウントを登録する</Text>
+        </Link>
+      </SimpleGrid>
+    </AuthRedirector>
   );
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -12,14 +12,13 @@ import {
   SimpleGrid,
   useToast,
 } from "@/components/chakra-ui/react";
-import { userAtom } from "@/utils/atom";
+import AuthRedirector from "@/components/client/AuthRedirector";
 import { initApp } from "@/utils/initApp";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { useRecoilState } from "recoil";
 
 type Form = {
   email: string;
@@ -38,169 +37,171 @@ export default function Register() {
   } = useForm<Form>();
 
   const [isShow, setIsShow] = useState(false);
-  const [user, setUser] = useRecoilState(userAtom);
 
   initApp();
   const auth = getAuth();
   const router = useRouter();
   const toast = useToast();
 
-  useEffect(() => {
-    if (user) router.replace("/");
-  });
-
   return (
-    <SimpleGrid
-      as="form"
-      placeContent="center"
-      placeItems="center"
-      gap={4}
-      p={4}
-      onSubmit={handleSubmit(async (data) => {
-        try {
-          await createUserWithEmailAndPassword(auth, data.email, data.password);
-          toast({
-            title: "アカウント登録が完了しました",
-            status: "error",
-            duration: 5000,
-            isClosable: true,
-          });
-          router.push("/");
-        } catch (error) {
-          toast({
-            title: `${error}`,
-            status: "error",
-            duration: 5000,
-            isClosable: true,
-          });
-        }
-      })}
-    >
-      <FormControl isInvalid={!!errors.email}>
-        <FormLabel>メールアドレス</FormLabel>
-        <Input
-          type="email"
-          placeholder="user@example.com"
-          {...register("email", {
-            required: "必須項目",
-            onBlur: () => {
-              if (getValues("emailConfirmation")) {
-                trigger("emailConfirmation");
-              }
-            },
-            pattern: {
-              value: /^[\w\-._]+@[\w\-._]+\.[A-Za-z]+/,
-              message: "入力形式がメールアドレスではありません",
-            },
-          })}
-        ></Input>
-        {errors.email && (
-          <FormErrorMessage>{errors.email.message}</FormErrorMessage>
-        )}
-      </FormControl>
-      <FormControl isInvalid={!!errors.emailConfirmation}>
-        <FormLabel>メールアドレスの確認</FormLabel>
-        <Input
-          type="email"
-          placeholder="user@example.com"
-          {...register("emailConfirmation", {
-            required: "必須項目",
-            pattern: {
-              value: /^[\w\-._]+@[\w\-._]+\.[A-Za-z]+/,
-              message: "入力形式がメールアドレスではありません",
-            },
-            validate: (value) => {
-              return (
-                value === getValues("email") || "メールアドレスが一致しません"
-              );
-            },
-          })}
-        ></Input>
-        {errors.emailConfirmation && (
-          <FormErrorMessage>
-            {errors.emailConfirmation.message}
-          </FormErrorMessage>
-        )}
-      </FormControl>
-      <FormControl isInvalid={!!errors.password}>
-        <FormLabel>パスワード</FormLabel>
-        <InputGroup>
+    <AuthRedirector isExists={true}>
+      <SimpleGrid
+        as="form"
+        placeContent="center"
+        placeItems="center"
+        gap={4}
+        p={4}
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await createUserWithEmailAndPassword(
+              auth,
+              data.email,
+              data.password
+            );
+            toast({
+              title: "アカウント登録が完了しました",
+              status: "error",
+              duration: 5000,
+              isClosable: true,
+            });
+            router.push("/");
+          } catch (error) {
+            toast({
+              title: `${error}`,
+              status: "error",
+              duration: 5000,
+              isClosable: true,
+            });
+          }
+        })}
+      >
+        <FormControl isInvalid={!!errors.email}>
+          <FormLabel>メールアドレス</FormLabel>
           <Input
-            maxLength={16}
-            type={isShow ? "text" : "password"}
-            {...register("password", {
+            type="email"
+            placeholder="user@example.com"
+            {...register("email", {
               required: "必須項目",
-              minLength: {
-                value: 8,
-                message: "8~16文字",
-              },
-              maxLength: {
-                value: 16,
-                message: "8~16文字",
-              },
               onBlur: () => {
-                if (getValues("passwordConfirmation")) {
-                  trigger("passwordConfirmation");
+                if (getValues("emailConfirmation")) {
+                  trigger("emailConfirmation");
                 }
+              },
+              pattern: {
+                value: /^[\w\-._]+@[\w\-._]+\.[A-Za-z]+/,
+                message: "入力形式がメールアドレスではありません",
               },
             })}
           ></Input>
-          <InputRightElement placeContent="center" placeItems="center">
-            <IconButton
-              variant="unstyled"
-              aria-label="show"
-              placeContent="center"
-              placeItems="center"
-              icon={isShow ? <ViewIcon /> : <ViewOffIcon />}
-              onClick={() => setIsShow(!isShow)}
-            />
-          </InputRightElement>
-        </InputGroup>
-        {errors.password && (
-          <FormErrorMessage>{errors.password.message}</FormErrorMessage>
-        )}
-      </FormControl>
-      <FormControl isInvalid={!!errors.passwordConfirmation}>
-        <FormLabel>パスワードの確認</FormLabel>
-        <InputGroup>
+          {errors.email && (
+            <FormErrorMessage>{errors.email.message}</FormErrorMessage>
+          )}
+        </FormControl>
+        <FormControl isInvalid={!!errors.emailConfirmation}>
+          <FormLabel>メールアドレスの確認</FormLabel>
           <Input
-            maxLength={16}
-            type={isShow ? "text" : "password"}
-            {...register("passwordConfirmation", {
+            type="email"
+            placeholder="user@example.com"
+            {...register("emailConfirmation", {
               required: "必須項目",
-              minLength: {
-                value: 8,
-                message: "8~16文字",
-              },
-              maxLength: {
-                value: 16,
-                message: "8~16文字",
+              pattern: {
+                value: /^[\w\-._]+@[\w\-._]+\.[A-Za-z]+/,
+                message: "入力形式がメールアドレスではありません",
               },
               validate: (value) => {
                 return (
-                  value === getValues("password") || "パスワードが一致しません"
+                  value === getValues("email") || "メールアドレスが一致しません"
                 );
               },
             })}
           ></Input>
-          <InputRightElement placeContent="center" placeItems="center">
-            <IconButton
-              variant="unstyled"
-              aria-label="show"
-              placeContent="center"
-              placeItems="center"
-              icon={isShow ? <ViewIcon /> : <ViewOffIcon />}
-              onClick={() => setIsShow(!isShow)}
-            />
-          </InputRightElement>
-        </InputGroup>
-        {errors.passwordConfirmation && (
-          <FormErrorMessage>
-            {errors.passwordConfirmation.message}
-          </FormErrorMessage>
-        )}
-      </FormControl>
-      <Button type="submit">Register</Button>
-    </SimpleGrid>
+          {errors.emailConfirmation && (
+            <FormErrorMessage>
+              {errors.emailConfirmation.message}
+            </FormErrorMessage>
+          )}
+        </FormControl>
+        <FormControl isInvalid={!!errors.password}>
+          <FormLabel>パスワード</FormLabel>
+          <InputGroup>
+            <Input
+              maxLength={16}
+              type={isShow ? "text" : "password"}
+              {...register("password", {
+                required: "必須項目",
+                minLength: {
+                  value: 8,
+                  message: "8~16文字",
+                },
+                maxLength: {
+                  value: 16,
+                  message: "8~16文字",
+                },
+                onBlur: () => {
+                  if (getValues("passwordConfirmation")) {
+                    trigger("passwordConfirmation");
+                  }
+                },
+              })}
+            ></Input>
+            <InputRightElement placeContent="center" placeItems="center">
+              <IconButton
+                variant="unstyled"
+                aria-label="show"
+                placeContent="center"
+                placeItems="center"
+                icon={isShow ? <ViewIcon /> : <ViewOffIcon />}
+                onClick={() => setIsShow(!isShow)}
+              />
+            </InputRightElement>
+          </InputGroup>
+          {errors.password && (
+            <FormErrorMessage>{errors.password.message}</FormErrorMessage>
+          )}
+        </FormControl>
+        <FormControl isInvalid={!!errors.passwordConfirmation}>
+          <FormLabel>パスワードの確認</FormLabel>
+          <InputGroup>
+            <Input
+              maxLength={16}
+              type={isShow ? "text" : "password"}
+              {...register("passwordConfirmation", {
+                required: "必須項目",
+                minLength: {
+                  value: 8,
+                  message: "8~16文字",
+                },
+                maxLength: {
+                  value: 16,
+                  message: "8~16文字",
+                },
+                validate: (value) => {
+                  return (
+                    value === getValues("password") ||
+                    "パスワードが一致しません"
+                  );
+                },
+              })}
+            ></Input>
+            <InputRightElement placeContent="center" placeItems="center">
+              <IconButton
+                variant="unstyled"
+                aria-label="show"
+                placeContent="center"
+                placeItems="center"
+                icon={isShow ? <ViewIcon /> : <ViewOffIcon />}
+                onClick={() => setIsShow(!isShow)}
+              />
+            </InputRightElement>
+          </InputGroup>
+          {errors.passwordConfirmation && (
+            <FormErrorMessage>
+              {errors.passwordConfirmation.message}
+            </FormErrorMessage>
+          )}
+        </FormControl>
+        <Button type="submit">Register</Button>
+      </SimpleGrid>
+    </AuthRedirector>
   );
 }
