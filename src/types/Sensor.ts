1ffa58312118d20bac7a9598aa9a639eacdbca75
shallow90125 <shallow90125@gmail.com>
2024-01-15 17:19:44 +0900

    refactor firebase

--- src/types/Sensor.ts
+++ src/types/Sensor.ts
@@ -1,6 +1,15 @@
-interface Sensor {
-  macAddress: string;
-  isOpen: boolean;
-  name: string;
-  uid: string;
-}
+import { z } from "zod";
+
+export const sensorSchema = z.object({
+  id: z.string(),
+  uid: z.string(),
+  name: z.string(),
+  history: z.array(
+    z.object({
+      timestamp: z.string(),
+      open: z.boolean(),
+    }),
+  ),
+});
+
+export type Sensor = z.infer<typeof sensorSchema>;
